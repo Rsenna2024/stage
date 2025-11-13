57ab36cb21d9ae63206580b83bb6ba0e7b4e0ae9
Senna Roest <2216421@talnet.nl>
2025-11-13 20:26:42 +0100

    Inlog pagina en een start aan CRUD

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
+use Termwind\Components\Raw;
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,6 +17,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::middleware('auth')->prefix('dashboard')->group(function () {
+        Route::resource('books', BookController::class);
+    });
 });
 
 require __DIR__.'/auth.php';
